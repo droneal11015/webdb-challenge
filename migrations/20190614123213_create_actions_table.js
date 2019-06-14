c3d5a0b132026219f564ead6b4b13f680fefb51a
droneal11015 <droneal11015@gmail.com>
2019-06-14 12:37:53 -0400

    completed migrations

---
+++ migrations/20190614123213_create_actions_table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('actions', function(actions) {
+      actions.increments();
+      actions.string('description', 255).notNullable();
+      actions.text('notes').notNullable();
+      actions.boolean('completed').defaultTo(false);
+  
+      actions
+        .integer('project_id').unsigned().notNullable()
+        .references('id').inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('actions');
+  };
+  
