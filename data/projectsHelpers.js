68c037a8c71079fcd2da577d49cddd67ea532038
droneal11015 <droneal11015@gmail.com>
2019-06-14 12:57:10 -0400

    added data files and helpers, setting up to incorporate stretch goals

---
+++ data/projectsHelpers.js
@@ -0,0 +1,41 @@
+const db = require('./dbConfig.js');
+
+module.exports = {
+
+  getProjects() {
+
+    let query = db('projects');
+    return query;
+  },
+
+  getProject(id) {
+
+    let project = db('projects').where({id})
+    let actions = db('actions').where({project_id: id})
+
+    return Promise.all([project, actions]).then(results => {
+      const [project, actions] = results;
+      return {...project, actions: [...actions]};
+    })
+  },
+
+  createProject({name, description, completed = false}){
+
+    let query = db('projects');
+    return query.insert({name, description, completed});
+  },
+
+  update(id, {name, description, completed}){
+
+    let query = db('projects');
+    return query.where({id})
+    .update({name, description, completed})
+  },
+
+  remove(id){
+
+    let query = db('projects');
+    return query.where({id})
+    .del()
+  },
+}
